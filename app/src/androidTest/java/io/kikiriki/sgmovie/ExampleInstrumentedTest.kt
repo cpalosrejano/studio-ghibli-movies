97b0488043e7a800df6119cadda61c643964a95b
Cristian Palos Rejano <cpalosrejano@gmail.com>
2023-10-20 11:24:39 +0200

    force unit test crash

--- app/src/androidTest/java/io/kikiriki/sgmovie/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/kikiriki/sgmovie/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.kikiriki.sgmovies", appContext.packageName)
+        assertEquals("io.kikiriki.sgmovie", appContext.packageName)
     }
 }
