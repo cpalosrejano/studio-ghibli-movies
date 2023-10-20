93661462458c5f07d414bc45c57ba6afebb7410f
Cristian Palos Rejano <cpalosrejano@gmail.com>
2023-10-20 09:28:06 +0200

    created actions for github

--- app/src/androidTest/java/io/kikiriki/sgmovie/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/kikiriki/sgmovie/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.kikiriki.notes", appContext.packageName)
+        assertEquals("io.kikiriki.sgmovie", appContext.packageName)
     }
 }
