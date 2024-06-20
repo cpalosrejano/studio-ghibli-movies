6ffe25734ac39e12d075a3dee68ff4f65d898ad4
Cristian Palos Rejano <cpalosrejano@gmail.com>
2024-06-20 17:06:54 +0200

    update version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         /* we will use the custom test runner which support hilt */
         //testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
