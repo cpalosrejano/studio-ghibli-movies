ce1f2c5596d8c63dd7a40e4b5e3ec0767b013c7c
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-06 18:26:20 +0100

    bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "io.kikiriki.sgmovie"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 20240306
+        versionName = "1.0.1"
 
         /* we will use the custom test runner which support hilt */
         //testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
