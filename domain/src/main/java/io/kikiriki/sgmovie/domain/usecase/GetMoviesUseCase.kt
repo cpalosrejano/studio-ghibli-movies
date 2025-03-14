3014a251e7a8e8859181ba5a28480f6c670dc32e
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-14 19:31:22 +0100

    sgm-65: use preference store in use case

--- domain/src/main/java/io/kikiriki/sgmovie/domain/usecase/GetMoviesUseCase.kt
+++ domain/src/main/java/io/kikiriki/sgmovie/domain/usecase/GetMoviesUseCase.kt
@@ -3,6 +3,7 @@ package io.kikiriki.sgmovie.domain.usecase
 import io.kikiriki.sgmovie.core.coroutines.di.IODispatcher
 import io.kikiriki.sgmovie.domain.model.Movie
 import io.kikiriki.sgmovie.domain.model.base.GResult
+import io.kikiriki.sgmovie.domain.preferences.PreferenceStorage
 import io.kikiriki.sgmovie.domain.repository.MovieRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
@@ -12,12 +13,26 @@ import javax.inject.Inject
 
 class GetMoviesUseCase @Inject constructor(
     private val movieRepository: MovieRepository,
+    private val preferenceStorage: PreferenceStorage,
     @IODispatcher private val dispatcher: CoroutineDispatcher
 ) {
 
     suspend operator fun invoke() : Flow<GResult<List<Movie>, Throwable>> = withContext(dispatcher) {
-        val lang = Locale.getDefault().language
-        return@withContext movieRepository.get(lang)
+
+        var forceRefresh = false
+        val systemLang = Locale.getDefault().language
+        val storedLang = preferenceStorage.getLanguage()
+
+        // save current system language and force refresh
+        if (storedLang == null || storedLang != systemLang) {
+            preferenceStorage.setLanguage(systemLang)
+            forceRefresh = true
+        }
+
+        return@withContext movieRepository.get(
+            forceRefresh = forceRefresh,
+            lang = systemLang
+        )
     }
 
 }
