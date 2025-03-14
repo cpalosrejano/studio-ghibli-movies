256de7b79f7f3583725c2e4a592e0c570e0a64fa
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-14 14:29:48 +0100

    sgm-59: fixed domain test

--- domain/src/test/java/io/kikiriki/sgmovie/domain/usecase/GetMoviesUseCaseTest.kt
+++ domain/src/test/java/io/kikiriki/sgmovie/domain/usecase/GetMoviesUseCaseTest.kt
@@ -65,30 +65,34 @@ class GetMoviesUseCaseTest : BaseTest() {
             Movie(
                 id = "dc2e6bd1-8156-4886-adff-b39e6043af0c",
                 title = "Spirited Away",
-                originalTitleRomanised = "Sen to Chihiro no kamikakushi",
-                image = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg",
-                movieBanner = "https://image.tmdb.org/t/p/original/bSXfU4dwZyBA1vMmXvejdRXBvuF.jpg",
+                titleRomanised = "Sen to Chihiro no kamikakushi",
+                imageCartel = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg",
+                imageBanner = "https://image.tmdb.org/t/p/original/bSXfU4dwZyBA1vMmXvejdRXBvuF.jpg",
                 description = "Spirited Away is an Oscar winning Japanese animated film about a ten year old girl who wanders away from her parents along a path that leads to a world ruled by strange and unusual monster-like animals. Her parents have been changed into pigs along with others inside a bathhouse full of these creatures. Will she ever see the world how it once was?",
                 director = "Hayao Miyazaki",
                 producer = "Toshio Suzuki",
+                soundtrack = "Joe Hisaishi",
                 releaseDate = 2001,
-                runningTime = "124",
+                runningTime = 124,
                 rtScore = 97,
-                favourite = true
+                coproduction = false,
+                like = true
             ),
             Movie(
                 id = "0440483e-ca0e-4120-8c50-4c8cd9b965d6",
                 title = "Princess Mononoke",
-                originalTitleRomanised = "Mononoke hime",
-                image = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/jHWmNr7m544fJ8eItsfNk8fs2Ed.jpg",
-                movieBanner = "https://image.tmdb.org/t/p/original/6pTqSq0zYIWCsucJys8q5L92kUY.jpg",
+                titleRomanised = "Mononoke hime",
+                imageCartel = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/jHWmNr7m544fJ8eItsfNk8fs2Ed.jpg",
+                imageBanner = "https://image.tmdb.org/t/p/original/6pTqSq0zYIWCsucJys8q5L92kUY.jpg",
                 description = "Ashitaka, a prince of the disappearing Ainu tribe, is cursed by a demonized boar god and must journey to the west to find a cure. Along the way, he encounters San, a young human woman fighting to protect the forest, and Lady Eboshi, who is trying to destroy it. Ashitaka must find a way to bring balance to this conflict.",
                 director = "Hayao Miyazaki",
                 producer = "Toshio Suzuki",
+                soundtrack = "Joe Hisaishi",
                 releaseDate = 1997,
-                runningTime = "134",
+                runningTime = 134,
                 rtScore = 92,
-                favourite = true
+                coproduction = false,
+                like = true
             ),
         )
 
