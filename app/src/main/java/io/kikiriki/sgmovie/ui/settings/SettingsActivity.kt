0f3a226fc64fe7e23c1faab3a29472c0451d2406
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-15 12:51:26 +0100

    sgm-61: add option to send email for request or help to translate the app

--- app/src/main/java/io/kikiriki/sgmovie/ui/settings/SettingsActivity.kt
+++ app/src/main/java/io/kikiriki/sgmovie/ui/settings/SettingsActivity.kt
@@ -1,17 +1,19 @@
 package io.kikiriki.sgmovie.ui.settings
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 import io.kikiriki.sgmovie.R
 import io.kikiriki.sgmovie.databinding.ActivitySettingsBinding
 import io.kikiriki.sgmovie.ui.BaseActivity
 import io.kikiriki.sgmovie.utils.Constants
-
+import java.util.Locale
 
 @AndroidEntryPoint
 class SettingsActivity : BaseActivity() {
@@ -35,7 +37,8 @@ class SettingsActivity : BaseActivity() {
     private fun setupView() {
         setSupportActionBar(viewBinding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        viewBinding.cardPrivacyPolicy.setOnClickListener { openPrivacyPolicy() }
+        viewBinding.lblPrivacyPolicy.setOnClickListener { openPrivacyPolicy() }
+        viewBinding.lblTranslateApp.setOnClickListener { translateApp() }
 
         try {
             val versionName: String = packageManager.getPackageInfo(packageName, 0).versionName.orEmpty()
@@ -43,13 +46,27 @@ class SettingsActivity : BaseActivity() {
         } catch (_:Exception) {
             viewBinding.appInfoTvVersion.visibility = View.GONE
         }
-
     }
 
     private fun openPrivacyPolicy() {
         val privacyPolicyUrl = Uri.parse(Constants.Legal.PRIVACY_POLICY_URL)
         val intent = Intent(Intent.ACTION_VIEW, privacyPolicyUrl)
         try { startActivity(intent) } catch (_ : Exception) {}
+    }
+
+    private fun translateApp() {
+        val locale = Locale.getDefault().toLanguageTag()
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(Constants.Contact.EMAIL))
+            putExtra(Intent.EXTRA_SUBJECT, getString(R.string.settings_lbl_translate_app_email_subject, locale))
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.settings_lbl_translate_app_email_body))
+        }
+        try {
+            startActivity(Intent.createChooser(intent, getString(R.string.settings_lbl_send_email)))
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, R.string.settings_lbl_error_no_email_client_found, Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
