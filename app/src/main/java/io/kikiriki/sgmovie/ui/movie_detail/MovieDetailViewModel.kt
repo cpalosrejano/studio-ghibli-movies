87b206609e6bb17dd0f902f674d6bb0daaa79a73
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-24 19:13:38 +0100

    sgm-75: update ui to show error or empty streaming providers

--- app/src/main/java/io/kikiriki/sgmovie/ui/movie_detail/MovieDetailViewModel.kt
+++ app/src/main/java/io/kikiriki/sgmovie/ui/movie_detail/MovieDetailViewModel.kt
@@ -6,16 +6,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.kikiriki.sgmovie.R
 import io.kikiriki.sgmovie.domain.model.Movie
+import io.kikiriki.sgmovie.domain.model.StreamingProvider
 import io.kikiriki.sgmovie.domain.usecase.GetMovieByIdUseCase
+import io.kikiriki.sgmovie.domain.usecase.GetStreamingProviderUseCase
 import io.kikiriki.sgmovie.domain.usecase.UpdateMovieLikeUseCase
 import io.kikiriki.sgmovie.utils.ExceptionManager
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import java.util.Locale
 import javax.inject.Inject
 
 class MovieDetailViewModel @Inject constructor(
+    private val getStreamingProviderUseCase: GetStreamingProviderUseCase,
     private val updateMovieLikeUseCase: UpdateMovieLikeUseCase,
     private val getMovieByIdUseCase: GetMovieByIdUseCase
 ) : ViewModel() {
@@ -26,10 +30,20 @@ class MovieDetailViewModel @Inject constructor(
     private val _movie = MutableLiveData<Movie>()
     val movie: LiveData<Movie> = _movie
 
+    private val _streamingProviders = MutableLiveData<List<StreamingProvider>?>(null)
+    val streamingProviders = _streamingProviders
+
+    private val _streamingProviderError: MutableLiveData<Int?> = MutableLiveData(null)
+    val streamingProviderError: LiveData<Int?> = _streamingProviderError
+
     fun getMovieById(movieId: String) = viewModelScope.launch {
         getMovieByIdUseCase(movieId)
             .onEach { movie ->
                 _movie.postValue(movie)
+                // fetch streaming provider if are still null
+                if (_streamingProviders.value == null) {
+                    fetchStreamingProviders(movie)
+                }
             }
             .catch {
                 val error = ExceptionManager.getMessage(it)
@@ -37,8 +51,6 @@ class MovieDetailViewModel @Inject constructor(
             }
             .collect()
     }
-
-
 
     fun updateMovieLike() = viewModelScope.launch {
         // check current movie is not null
@@ -66,4 +78,17 @@ class MovieDetailViewModel @Inject constructor(
         )
     }
 
+    private fun fetchStreamingProviders(movie: Movie) = viewModelScope.launch {
+        val countryCode = Locale.getDefault().country
+        getStreamingProviderUseCase(movie, countryCode).fold(
+            onSuccess = { data ->
+                _streamingProviders.postValue(data)
+            },
+            onFailure = { failure ->
+                val error = ExceptionManager.getMessage(failure)
+                _streamingProviderError.postValue(error)
+            }
+        )
+
+    }
 }
