5d395d80fc2ce354efea74e71350db6d146949ad
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-24 19:18:38 +0100

    sgm-75: update movies cache to 12 hours and refresh when system language changes

--- domain/src/main/java/io/kikiriki/sgmovie/domain/usecase/GetMoviesUseCase.kt
+++ domain/src/main/java/io/kikiriki/sgmovie/domain/usecase/GetMoviesUseCase.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import java.util.Locale
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class GetMoviesUseCase @Inject constructor(
@@ -17,21 +18,33 @@ class GetMoviesUseCase @Inject constructor(
     @IODispatcher private val dispatcher: CoroutineDispatcher
 ) {
 
+    private val cacheTime = TimeUnit.HOURS.toMillis(12)
+
     suspend operator fun invoke() : Flow<GResult<List<Movie>, Throwable>> = withContext(dispatcher) {
 
-        var forceRefresh = false
-        val systemLang = Locale.getDefault().toLanguageTag()
-        val storedLang = preferenceStorage.getLanguage()
+        // get saved data
+        val savedLanguage = preferenceStorage.getLanguage()
+        val lastRequestTimestamp = preferenceStorage.getTimestampLastRequest()
 
-        // save current system language and force refresh
-        if (storedLang == null || storedLang != systemLang) {
-            preferenceStorage.setLanguage(systemLang)
-            forceRefresh = true
+        // get current data
+        val currentLanguage = Locale.getDefault().toLanguageTag()
+        val currentTimeMillis = System.currentTimeMillis()
+
+        // check if we need to refresh data from api
+        val shouldRefreshData =
+            savedLanguage == null ||
+            savedLanguage != currentLanguage ||
+            (currentTimeMillis - lastRequestTimestamp) > cacheTime
+
+        // update preferences if we need to refresh
+        if (shouldRefreshData) {
+            preferenceStorage.setLanguage(currentLanguage)
+            preferenceStorage.setTimestampLastRequest(currentTimeMillis)
         }
 
         return@withContext movieRepository.get(
-            forceRefresh = forceRefresh,
-            lang = systemLang
+            forceRefresh = shouldRefreshData,
+            lang = currentLanguage
         )
     }
 
