65ad56cf71735cb592a287cde652c9ebf0f556ca
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-24 19:28:39 +0100

    sgm-75: Refactor: Add TMDB endpoints and streaming provider exception
    - Add `STREAMING_PROVIDER_NOT_FOUND` exception.
    - Add TMDB endpoints.
    - Update `MovieEndpoints` base URL.

--- data/src/main/java/io/kikiriki/sgmovie/data/utils/Constants.kt
+++ data/src/main/java/io/kikiriki/sgmovie/data/utils/Constants.kt
@@ -5,7 +5,8 @@ object Constants {
     // Data repository constants
     object Repository {
         const val MOCK = false
-        const val URL = "https://studio-ghibli-movies-api.vercel.app/"
+        const val URL_API_MOVIE = "https://studio-ghibli-movies-api.vercel.app/"
+        const val URL_API_TMDB = "https://api.themoviedb.org/3/"
     }
 
 }
