ef5485c61d505b9d7988b2fe893223e416e6c94b
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-26 19:34:21 +0100

    sgm-75: implement changes made in domain layer: handle rent and buy options in tmdb api

--- data/src/main/java/io/kikiriki/sgmovie/data/model/tmdb/WatchProviderMapper.kt
+++ data/src/main/java/io/kikiriki/sgmovie/data/model/tmdb/WatchProviderMapper.kt
@@ -1,20 +1,28 @@
 package io.kikiriki.sgmovie.data.model.tmdb
 
 import io.kikiriki.sgmovie.domain.model.StreamingProvider
+import io.kikiriki.sgmovie.domain.model.WatchProviders
 
 object WatchProviderMapper {
 
-    fun remoteToDomain(remote: Map<String, CountryWatchProviderRemote>) : Map<String, List<StreamingProvider>> {
+    fun remoteToDomain(remote: Map<String, CountryWatchProviderRemote>) : Map<String, WatchProviders> {
         return remote.mapValues { entry ->
-            entry.value.flatrate?.map { providerRemote ->
-                StreamingProvider(
-                    id = providerRemote.provider_id,
-                    name = providerRemote.provider_name,
-                    logo = "https://image.tmdb.org/t/p/w500" + providerRemote.logo_path
-                )
-            } ?: emptyList()
+            val countryProviders = entry.value
+            WatchProviders(
+                link = countryProviders.link,
+                flatrate = countryProviders.flatrate?.map { it.toDomain() } ?: emptyList(),
+                rent = countryProviders.rent?.map { it.toDomain() } ?: emptyList(),
+                buy = countryProviders.buy?.map { it.toDomain() } ?: emptyList()
+            )
         }
     }
 
+    private fun StreamingProviderRemote.toDomain(): StreamingProvider {
+        return StreamingProvider(
+            id = provider_id,
+            name = provider_name,
+            logo = "https://image.tmdb.org/t/p/w500$logo_path"
+        )
+    }
 
 }
