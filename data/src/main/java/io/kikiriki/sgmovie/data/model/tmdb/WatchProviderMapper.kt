c79dc70a0f56594547f557d03377270aac5699e9
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-27 20:04:27 +0100

    sm-76: feat(watch_providers): add type to StreamingProvider

--- data/src/main/java/io/kikiriki/sgmovie/data/model/tmdb/WatchProviderMapper.kt
+++ data/src/main/java/io/kikiriki/sgmovie/data/model/tmdb/WatchProviderMapper.kt
@@ -10,18 +10,19 @@ object WatchProviderMapper {
             val countryProviders = entry.value
             WatchProviders(
                 link = countryProviders.link,
-                flatrate = countryProviders.flatrate?.map { it.toDomain() } ?: emptyList(),
-                rent = countryProviders.rent?.map { it.toDomain() } ?: emptyList(),
-                buy = countryProviders.buy?.map { it.toDomain() } ?: emptyList()
+                flatrate = countryProviders.flatrate?.map { it.toDomain(StreamingProvider.Type.FLATRATE) } ?: emptyList(),
+                rent = countryProviders.rent?.map { it.toDomain(StreamingProvider.Type.RENT) } ?: emptyList(),
+                buy = countryProviders.buy?.map { it.toDomain(StreamingProvider.Type.BUY) } ?: emptyList()
             )
         }
     }
 
-    private fun StreamingProviderRemote.toDomain(): StreamingProvider {
+    private fun StreamingProviderRemote.toDomain(type: StreamingProvider.Type): StreamingProvider {
         return StreamingProvider(
             id = provider_id,
             name = provider_name,
-            logo = "https://image.tmdb.org/t/p/w500$logo_path"
+            logo = "https://image.tmdb.org/t/p/w500$logo_path",
+            type = type
         )
     }
 
