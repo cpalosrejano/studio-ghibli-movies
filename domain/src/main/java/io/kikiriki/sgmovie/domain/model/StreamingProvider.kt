6c15205b7c7dad616a25a7725f054468cb0e701e
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-27 20:04:27 +0100

    sm-76: feat(watch_providers): add type to StreamingProvider

--- domain/src/main/java/io/kikiriki/sgmovie/domain/model/StreamingProvider.kt
+++ domain/src/main/java/io/kikiriki/sgmovie/domain/model/StreamingProvider.kt
@@ -3,8 +3,13 @@ package io.kikiriki.sgmovie.domain.model
 data class StreamingProvider(
     val id: Int,
     val name: String,
-    val logo: String
-)
+    val logo: String,
+    val type: Type
+) {
+    enum class Type {
+        FLATRATE, RENT, BUY
+    }
+}
 
 data class WatchProviders(
     val link: String? = null,
