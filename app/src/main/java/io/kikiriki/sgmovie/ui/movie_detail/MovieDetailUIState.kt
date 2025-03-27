ec759d966bb833a7b36b66d6979d064b21d8ee4b
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-27 20:06:54 +0100

    sgm-76: add analytics events to MovieDetailFragment

--- app/src/main/java/io/kikiriki/sgmovie/ui/movie_detail/MovieDetailUIState.kt
+++
@@ -1,9 +0,0 @@
-package io.kikiriki.sgmovie.ui.movie_detail
-
-import io.kikiriki.sgmovie.domain.model.Movie
-
-data class MovieDetailUIState(
-    val error: Int? = null,
-    val movie: Movie? = null
-)
-
