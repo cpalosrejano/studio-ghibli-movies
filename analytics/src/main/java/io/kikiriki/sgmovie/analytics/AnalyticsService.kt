b273774172de2374935456e7cdd95a0ba9397f90
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-27 20:04:00 +0100

    sgm-76: feat(analytics): add logScreenView, update AnalyticEvent and FirebaseAnalyticsService

--- analytics/src/main/java/io/kikiriki/sgmovie/analytics/AnalyticsService.kt
+++ analytics/src/main/java/io/kikiriki/sgmovie/analytics/AnalyticsService.kt
@@ -1,6 +1,7 @@
 package io.kikiriki.sgmovie.analytics
 
 interface AnalyticsService {
+    fun logScreenView(screenName: String, screenClass: String)
     fun logEvent(event: AnalyticEvent, params: Map<String, Any?> = emptyMap())
     fun setUserId(userId: String?)
     fun setUserProperty(name: String, value: String?)
--- analytics/src/main/java/io/kikiriki/sgmovie/analytics/firebase/FirebaseAnalyticsService.kt
+++ analytics/src/main/java/io/kikiriki/sgmovie/analytics/firebase/FirebaseAnalyticsService.kt
@@ -2,6 +2,7 @@ package io.kikiriki.sgmovie.analytics.firebase
 
 import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.logEvent
 import io.kikiriki.sgmovie.analytics.AnalyticEvent
 import io.kikiriki.sgmovie.analytics.AnalyticsService
 import javax.inject.Inject
@@ -11,7 +12,27 @@ class FirebaseAnalyticsService @Inject constructor(
 ) : AnalyticsService {
 
     override fun logEvent(event: AnalyticEvent, params: Map<String, Any?>) {
-        val bundle = Bundle().apply {
+        val bundle = getBundleFromParams(params)
+        firebaseAnalytics.logEvent(event.eventName, bundle)
+    }
+
+    override fun logScreenView(screenName: String, screenClass: String) {
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
+            param(FirebaseAnalytics.Param.SCREEN_CLASS, screenClass)
+        }
+    }
+
+    override fun setUserId(userId: String?) {
+        firebaseAnalytics.setUserId(userId)
+    }
+
+    override fun setUserProperty(name: String, value: String?) {
+        firebaseAnalytics.setUserProperty(name, value)
+    }
+
+    private fun getBundleFromParams(params: Map<String, Any?>) : Bundle {
+        return Bundle().apply {
             params.forEach { (key, value) ->
                 when (value) {
                     is String -> putString(key, value)
@@ -22,14 +43,5 @@ class FirebaseAnalyticsService @Inject constructor(
                 }
             }
         }
-        firebaseAnalytics.logEvent(event.name, bundle)
-    }
-
-    override fun setUserId(userId: String?) {
-        firebaseAnalytics.setUserId(userId)
-    }
-
-    override fun setUserProperty(name: String, value: String?) {
-        firebaseAnalytics.setUserProperty(name, value)
     }
 }
