aec12a5fab2d6a73680268eda2e4b101a0d70f02
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-28 16:23:21 +0100

    feat: add remote config with Firebase implementation

---
+++ data/src/main/java/io/kikiriki/sgmovie/data/di/RemoteConfigModule.kt
@@ -0,0 +1,47 @@
+package io.kikiriki.sgmovie.data.di
+
+import com.google.firebase.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.remoteConfig
+import com.google.firebase.remoteconfig.remoteConfigSettings
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.kikiriki.sgmovie.data.preferences.RemoteConfigImpl
+import io.kikiriki.sgmovie.domain.preferences.RemoteConfig
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AbsRemoteConfigModule {
+
+    @Binds
+    abstract fun bindRemoteConfig(implementation: RemoteConfigImpl) : RemoteConfig
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteConfigModule {
+
+    @Provides
+    fun provideFirebaseRemoteConfig() : FirebaseRemoteConfig {
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        remoteConfig.setConfigSettingsAsync(remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        })
+        remoteConfig.setDefaultsAsync(getDefaultValues())
+        return remoteConfig
+    }
+
+    private fun getDefaultValues() : Map<String, Any> {
+        return mapOf(
+            RemoteConfig.MOVIE_LIKES_REFRESH_IN_SECONDS to 120,  // 2 minutes
+            RemoteConfig.API_CACHE_HOURS to 48,             // 12 hours
+            RemoteConfig.ENABLE_PAYPAL to true,             // enable paypal by default
+            RemoteConfig.ENABLE_MAINTENANCE to false        // disable maintenance by default
+        )
+    }
+
+
+}
