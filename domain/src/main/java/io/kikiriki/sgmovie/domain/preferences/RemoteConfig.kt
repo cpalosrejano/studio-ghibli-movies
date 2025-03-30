c2a93e6a722c75459226ad0c5ed2f5b5aae1fc3d
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-30 13:22:39 +0200

    sgm-90: feat: show contact layout based on remote config value

--- domain/src/main/java/io/kikiriki/sgmovie/domain/preferences/RemoteConfig.kt
+++ domain/src/main/java/io/kikiriki/sgmovie/domain/preferences/RemoteConfig.kt
@@ -6,10 +6,14 @@ interface RemoteConfig {
         const val API_CACHE_HOURS = "api_cache_hours"
         const val ENABLE_PAYPAL = "enable_paypal"
         const val ENABLE_MAINTENANCE = "enable_maintenance"
+        const val ENABLE_CONTACT = "enable_contact"
+        const val MIN_APP_VERSION = "config_min_app_version"
     }
 
     suspend fun getFirestoreRefreshSeconds(): Long
     suspend fun getApiCacheHour(): Long
     suspend fun isPaypalEnabled(): Boolean
     suspend fun isMaintenanceEnabled(): Boolean
+    suspend fun getMinAppVersion() : Long
+    suspend fun isContactEnabled() : Boolean
 }
