066349739bd7fcd738b728af676b8b0e6e6fea12
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-04-01 20:38:36 +0200

    sgm-84: feat: Movie Likes
    - Changed `getLikes()` to be a suspend function returning a `Map<String, Long>`.
    - Added `updateAllLikes` to `MovieLocalDataSource` to update likes in local database.

--- data/src/main/java/io/kikiriki/sgmovie/data/repository/movie/firestore/MovieFirestoreDataSource.kt
+++ data/src/main/java/io/kikiriki/sgmovie/data/repository/movie/firestore/MovieFirestoreDataSource.kt
@@ -3,7 +3,7 @@ package io.kikiriki.sgmovie.data.repository.movie.firestore
 import kotlinx.coroutines.flow.Flow
 
 interface MovieFirestoreDataSource {
-    fun getLikes(): Flow<Map<String, Long>>
+    suspend fun getLikes(): Map<String, Long>
     fun getMovieLikesById(movieId: String) : Flow<Long>
     fun updateLike(movieId: String, like: Boolean)
 }
