03380903fcac3514bb57b00ba83363e6bb578ba9
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-04-01 20:42:41 +0200

    sgm-84: feat: Update like status and count for movies
    This commit modifies the app to correctly update the like status and like count for movies.
    
    Changes include:
    
    - Updated `UpdateMovieLikeUseCase` to toggle the like status of a movie and adjust the like count accordingly.
    - Modified `MainViewModel` and `MovieDetailViewModel` to utilize the updated `UpdateMovieLikeUseCase` for movie like status changes.
    - The like count is now incremented or decremented based on the new like status (liked or unliked).

--- domain/src/main/java/io/kikiriki/sgmovie/domain/usecase/UpdateMovieLikeUseCase.kt
+++ domain/src/main/java/io/kikiriki/sgmovie/domain/usecase/UpdateMovieLikeUseCase.kt
@@ -13,7 +13,10 @@ class UpdateMovieLikeUseCase @Inject constructor(
 ) {
 
     suspend operator fun invoke(movie: Movie) : Result<Boolean> = withContext(dispatcher) {
-        return@withContext movieRepository.updateLike(movie)
+        val newLikeStatus = !movie.like
+        val newLikeCountStatus = movie.likeCount + if (newLikeStatus) +1 else -1
+        val newMovieStatus = movie.copy(like = newLikeStatus, likeCount = newLikeCountStatus)
+        return@withContext movieRepository.updateLike(newMovieStatus)
     }
 
 }
