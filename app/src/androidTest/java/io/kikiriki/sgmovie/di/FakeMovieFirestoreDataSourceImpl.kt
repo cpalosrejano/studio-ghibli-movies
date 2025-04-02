382a25cdd1bc8fabf3ae9b2571457e065a4cf73e
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-04-02 20:39:58 +0200

    sgm-84: ui test fixed
    This commit introduces local storage for movie likes, enhancing the app's ability to retain user preferences even when offline or during network disruptions.
    
    New Features:
    - Implemented local storage using DataStore to persist movie likes.
    - Added functionality to update and retrieve movie likes from the local storage.
    - Modified `FakeMovieFirestoreDataSourceImpl` to store likes locally during testing.
    
    Enhancements:
    - Updated the app to store movie likes locally.
    - Refactored code to seamlessly integrate local storage with the existing data flow.
    
    Testing:
    - Updated UI tests to ensure movie likes are correctly stored and retrieved from the local storage.

--- app/src/androidTest/java/io/kikiriki/sgmovie/di/FakeMovieFirestoreDataSourceImpl.kt
+++ app/src/androidTest/java/io/kikiriki/sgmovie/di/FakeMovieFirestoreDataSourceImpl.kt
@@ -7,23 +7,24 @@ import javax.inject.Inject
 
 class FakeMovieFirestoreDataSourceImpl @Inject constructor() : MovieFirestoreDataSource {
 
-    private val moviesLike = hashMapOf<String, Long>(
-        "some-id" to 3,
-        "another-id" to 1
+    private val storedLikes = mutableMapOf(
+        "758bf02e-3122-46e0-884e-67cf83df1786" to 4L,
+        "dc2e6bd1-8156-4886-adff-b39e6043af0c" to 2L,
+        "58611129-2dbc-4a81-a72f-77ddfc1b1b49" to 3L
     )
 
-    override fun getLikes(): Flow<Map<String, Long>> = flowOf(moviesLike)
+    override suspend fun getLikes(): Map<String, Long> = storedLikes
 
     override fun getMovieLikesById(movieId: String): Flow<Long> {
         return flowOf(1)
     }
 
     override fun updateLike(movieId: String, like: Boolean) {
-        if(moviesLike.containsKey(movieId)) {
-            val actualLikes = moviesLike[movieId] ?: 0
-            moviesLike[movieId] = actualLikes + (if (like) 1 else -1)
+        if(storedLikes.containsKey(movieId)) {
+            val actualLikes = storedLikes[movieId] ?: 0
+            storedLikes[movieId] = actualLikes + (if (like) 1 else -1)
         } else {
-            moviesLike[movieId] = 1
+            storedLikes[movieId] = 1
         }
     }
 }
