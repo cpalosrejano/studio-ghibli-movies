799df1bdec5e7d19eeef449ea7ed7b57ea481b76
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-04-04 19:28:09 +0200

    sgm-100: refactor: Rename MovieRemoteDataSource to MovieRemoteVercelDataSource
    This commit refactors the `MovieRemoteDataSource` to `MovieRemoteVercelDataSource` to better reflect its specific implementation and origin (Vercel).
    
    - Updated the interface name and its implementation class name accordingly.
    - Adjusted the binding in the dependency injection module.
    - Updated the repository implementation to use the new data source name.
    
    This change improves code clarity and maintainability by distinguishing this specific remote data source implementation.

--- data/src/main/java/io/kikiriki/sgmovie/data/repository/movie/remoteVercel/MovieRemoteVercelDataSource.kt
+++ data/src/main/java/io/kikiriki/sgmovie/data/repository/movie/remoteVercel/MovieRemoteVercelDataSource.kt
@@ -2,6 +2,6 @@ package io.kikiriki.sgmovie.data.repository.movie.remoteVercel
 
 import io.kikiriki.sgmovie.data.model.movie.MovieRemote
 
-interface MovieRemoteDataSource {
+interface MovieRemoteVercelDataSource {
     suspend fun get(lang: String, coproductions: Boolean = false) : List<MovieRemote>
 }
