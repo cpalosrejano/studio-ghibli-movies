562238e937a3b4b9cc8397c07f8355a9ff1b8e38
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-04-04 20:04:31 +0200

    sgm-100: add API render url
    This commit updates the API URLs used in the application. The main movie API URL has been changed from "https://studio-ghibli-movies-api.vercel.app/" to "https://studio-ghibli-movies-api-0yrc.onrender.com/". The old URL is now defined as `URL_API_MOVIE_VERCEL` while the new URL is defined as `URL_API_MOVIE_RENDER`.

--- data/src/main/java/io/kikiriki/sgmovie/data/utils/Constants.kt
+++ data/src/main/java/io/kikiriki/sgmovie/data/utils/Constants.kt
@@ -5,7 +5,8 @@ object Constants {
     // Data repository constants
     object Repository {
         const val MOCK = false
-        const val URL_API_MOVIE = "https://studio-ghibli-movies-api.vercel.app/"
+        const val URL_API_MOVIE_VERCEL = "https://studio-ghibli-movies-api.vercel.app/"
+        const val URL_API_MOVIE_RENDER = "https://studio-ghibli-movies-api-0yrc.onrender.com/"
         const val URL_API_TMDB = "https://api.themoviedb.org/3/"
     }
 
