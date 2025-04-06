1461e9b75d0155484e2563bf14e87d8dcc738c50
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-04-06 09:14:57 +0200

    sgm-103: Update database schema with new fields
    This commit updates the database schema to include new fields in the `movies` table.
    
    Specifically, the following changes were made:
    
    - Added `tmdb_id` column to the `movies` table in migration 2 to 3.
    - Added `like_count` column to the `movies` table in migration 3 to 4.
    - Updated the database version to 4 in `AppDatabase`.
    - Added the new migration (3 to 4) to the database builder in `DatabaseModule`.

--- data/src/main/java/io/kikiriki/sgmovie/data/AppDatabase.kt
+++ data/src/main/java/io/kikiriki/sgmovie/data/AppDatabase.kt
@@ -6,7 +6,7 @@ import io.kikiriki.sgmovie.data.model.movie.MovieLocal
 import io.kikiriki.sgmovie.data.repository.movie.local.MovieDao
 
 @Database(
-    version = 3,
+    version = 4,
     entities = [MovieLocal::class]
 )
 abstract class AppDatabase : RoomDatabase() {
